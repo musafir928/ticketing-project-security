476b6a9edf42197892e4db932e0cbaf281a9794f
musafir928 <uganoghli@gmail.com>
2022-05-26 23:48:14 +0200

    Revert "Security"
    This reverts commit 14a6bb9bd526ac369da7c083e50d99f405c156f3.

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import org.springframework.security.core.userdetails.User;
@@ -10,14 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-public class SecurityConfig { //BREAK TILL 8:45
+public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
@@ -27,60 +25,9 @@ public class SecurityConfig { //BREAK TILL 8:45
 
         userList.add(
                 new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-        userList.add(
-                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")));
 
 
         return new InMemoryUserDetailsManager(userList);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        return http
-                .authorizeRequests()
-                .antMatchers("/user/**").hasRole("ADMIN")
-                .antMatchers("/project/**").hasRole("MANAGER")
-                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
-                .antMatchers("/task/**").hasRole("MANAGER")
-//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
-//                .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")
-
-                .antMatchers(
-                       "/",
-                       "/login",
-                       "/fragments/**",
-                       "/assets/**",
-                       "/images/**"
-                ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-//                .httpBasic()
-                .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/welcome")
-                    .failureUrl("/login?error=true")
-                    .permitAll()
-                .and().build();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
